41cfac8ddd7fba1cdc508125cfad350aac38269a
RadostW <radost.waszkiewicz@gmail.com>
2023-02-22 15:02:53 +0100

    Setup prersonal website.
    - move header and footer to js script

---
+++ js/menu.js
@@ -0,0 +1,23 @@
+var menu = `
+<nav class="nav nav-masthead justify-content-center">
+    <a class="nav-link" id="index" href="./index.html">Home</a>
+    <a class="nav-link" id="calculators" href="calculators.html">Calculators and tools</a>
+    <a class="nav-link" id="publications"
+        href="https://scholar.google.com/citations?user=zhkj1RUAAAAJ">Publications</a>
+    <a class="nav-link" id="other" href="otherthings.html">Things that changed my
+        mind</a>
+</nav>
+`
+
+var footer = `
+<a href="https://scholar.google.com/citations?user=zhkj1RUAAAAJ">Google Scholar</a>,
+<a href="http://orcid.org/0000-0002-0376-1708">ORCID</a>,
+<a href="http://fb.com/radost.waszkiewicz">Facebook</a>
+`
+
+function setup_header_and_footer(active)
+{
+    document.getElementById('menu-div').innerHTML = menu;
+    document.getElementById(active).classList.add('active');
+    document.getElementById('footer-div').innerHTML = footer;
+}
